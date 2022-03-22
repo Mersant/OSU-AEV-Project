b785af9f3616d176ea1924837246aafa458e23b6
Litoncinnamon <102173297+Litoncinnamon@users.noreply.github.com>
2022-03-22 12:33:26 -0400

    Delete PasteData.m

--- PasteData.m
+++
@@ -1,8 +0,0 @@
-function RetData = PasteData(DataSet, DataName, Data)
-%Searches for 'DataName' in 'DataSet' and pastes 'Data' below it
-
-[row, col] = find(strcmp(DataSet,DataName));
-DataSet( row+1:(row+length(Data)),col ) = Data;
-
-RetData = DataSet;
-end
